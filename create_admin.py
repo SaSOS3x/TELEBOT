7a12ff10e6190687378bfb52f6cceebc98e98972
Your Name <you@example.com>
2023-04-12 15:33:24 +0300

    same

--- create_admin.py
+++ create_admin.py
@@ -4,7 +4,7 @@ import sqlite3
 db = sqlite3.connect('base_pyramid.db',check_same_thread=False)
 cursor = db.cursor()
 
-login = hashlib.md5(input('login: ').encode('utf-8')).hexdigest()
+login = input('login: ')
 password =hashlib.md5(input('password: ').encode('utf-8')).hexdigest()
 
 print(f'login: {login}\npassword: {password}')
