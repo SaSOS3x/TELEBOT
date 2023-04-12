35926092d6ee10e3e2b566a52ed1308aa9028c91
SaSOS3x <sanekmrd789@gmail.com>
2023-04-12 10:52:01 +0300

    gfgf

--- menu.py
+++ menu.py
@@ -25,3 +25,25 @@ functions_access_no.add(
     types.InlineKeyboardButton(text='def1(no access)', callback_data='def_test'),
     types.InlineKeyboardButton(text='❌', callback_data='back')
 )
+
+
+
+menu_main = types.InlineKeyboardMarkup(row_width=2)
+menu_main.add(
+    types.InlineKeyboardButton(text='Профиль', callback_data='profile'),
+    types.InlineKeyboardButton(text='О компании', callback_data='information'),
+    types.InlineKeyboardButton(text='Обучение', callback_data='learning'),
+    types.InlineKeyboardButton(text='База знаний', callback_data='knowelege_base')
+)
+menu_start = types.InlineKeyboardMarkup(row_width=2)
+menu_start.add(
+    types.InlineKeyboardButton(text='Я новый сотрудник', callback_data='new'),
+    types.InlineKeyboardButton(text='Я уже работаю', callback_data='no_new')
+)
+menu_next = types.InlineKeyboardMarkup(row_width=2)
+menu_next.add(
+    types.InlineKeyboardButton(text='Пиццемейкер', callback_data='pizza_maker'),
+    types.InlineKeyboardButton(text='Кассир', callback_data='cassier'),
+    types.InlineKeyboardButton(text='Инструктор', callback_data='instructor'),
+    types.InlineKeyboardButton(text='Менеджер', callback_data='manager')
+)
